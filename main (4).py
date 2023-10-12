9606bbbd32706494fc8f353964f2036c1d364f3e
Ramshad7798 <147702940+Ramshad7798@users.noreply.github.com>
2023-10-12 14:30:37 +0530

    Add files via upload

---
+++ main (4).py
@@ -0,0 +1,8 @@
+def linear_search_product(product_list, target_product):…
+
+# Sample usage
+if __name__ == "__main__":
+    products = ["apple", "banana", "orange", "apple", "grape"]
+    target = "apple"
+    result = linear_search_product(products, target)
+    print(result)
